b78916b954415209e758d46d16028b1d6dcbcd38
zy0n <zy0n.barto@gmail.com>
2021-03-01 23:01:10 -0500

    duh

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -43,7 +43,7 @@ export const darkColors: Colors = {
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
-  input: "#483f5a",
+  input: "#27252f",
   primaryDark: "#0098A1",
   tertiary: "#353547",
   text: "#EAE2FC",
