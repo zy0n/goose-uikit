2ecddf7705d593db36789932d04cf5c42082b41b
zy0n <zy0n.barto@gmail.com>
2021-03-01 22:43:29 -0500

    hah

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -38,7 +38,7 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#607d8b",
+  secondary: "#ff9800",
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
