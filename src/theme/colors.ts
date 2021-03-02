127f400d13b0e66fb623b03bcc161d14ea55f4d3
zy0n <zy0n.barto@gmail.com>
2021-03-01 19:34:12 -0500

    change colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -25,7 +25,7 @@ export const lightColors: Colors = {
   tertiary: "#EFF4F5",
   text: "#452A7A",
   textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
+  textSubtle: "#000000",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
