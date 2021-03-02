f73feecf052060bc41edf5c7d0b817e820880cc6
zy0n <zy0n.barto@gmail.com>
2021-03-02 00:19:06 -0500

    colored toggles better

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -25,7 +25,7 @@ export const lightColors: Colors = {
   tertiary: "#EFF4F5",
   text: "#aa8929",
   textDisabled: "#BDC2C4",
-  textSubtle: "#694d25",
+  textSubtle: "#080602",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   modal: "#aa8929",
